b9d4b8ab06753cd9749c79f5cc79d1b7f51d706d
SHRAVANGT <59924740+SHRAVANGT@users.noreply.github.com>
2024-07-27 11:40:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+import project1 from "./assets/projects/budget-f1.png";
+import project2 from "./assets/projects/Note-taking01.png";
+import project3 from "./assets/projects/portfolio.png";
+import project4 from "./assets/projects/quiz-right.png";
+import project5 from "./assets/projects/calc.png";
+
+export const INTRO = `I am a passionate web developer with a knack for crafting robust and scalable web applications. With many projects I have honed my skills in front-end technologies like HTML,CSS,JS and React. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+//chnage it
+export const ABOUT_TEXT = `I am a dedicated and versatile Web developer with a passion for creating efficient and user-friendly web applications. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = `While I may not have direct professional experience in this field, but I have completed many projects that demonstrate my skills and dedication.I am eager to apply my knowledge and grow with a company that values innovation and hardwork`;
+
+export const PROJECTS = [
+  {
+    title: "Budget",
+    image: project1,
+    description:
+      "A Budget website with features like adding and deleting budget , expenses with Total.",
+    technologies: ["HTML", "CSS", "React", "Javascript"],
+  },
+  {
+    title: "Note-taking",
+    image: project2,
+    description:
+      "A website for Note taking with markdown with features like creating,editing,deleting the Note and Tag .",
+    technologies: ["HTML", "CSS", "Javascript", "React"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "tailwind", "framer-motion"],
+  },
+  {
+    title: "Quiz",
+    image: project4,
+    description:
+      "A Quiz website with functions like color changing based on answer ",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Calculator",
+    image: project5,
+    description:
+      "A simple calculator with basic math operations like addition,subtraction,division,multiplication  ",
+    technologies: ["HTML", "CSS", "Javascript", "React"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kumta,Karnataka-581332 ",
+  phoneNo: "+91 7975440783 ",
+  email: "gtatalex@gmail.com",
+};
